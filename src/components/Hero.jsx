41f3d82eaa0679d86f68c95c77153bc59e7f1397
Akash Patil <akashpatil2002@gmail.com>
2025-06-07 10:47:59 -0400

    Resume link fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,12 +23,11 @@ const Hero = () => {
         </p>
 
         <div className="mt-8 flex gap-4">
-          <a href="/certificates/akashofficiall.pdf" target="_blank" rel="noopener noreferrer">
-
+          <a href="/certificates/akashofficiall.pdf" target="_blank" rel="noopener noreferrer"/>
             <button className="bg-redwood text-white px-6 py-3 rounded-full font-medium shadow-lg hover:bg-cordovan hover:shadow-xl transition hover:scale-105 duration-300">
               Resume
             </button>
-          </Link>
+          
 
           <Link to="projects" smooth={true} duration={500}>
             <button className="border border-redwood text-redwood px-6 py-3 rounded-full font-medium hover:bg-redwood hover:text-white transition hover:scale-105 duration-300">
